09c1fac6c6c88387660ab57f0c47962208e51766
tau <tauwoo@seuxw.cc>
2019-06-13 11:19:29 +0800

    [feat] add async-request with POST method

--- request_base.py
+++ request_base.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from aiohttp import ClientSession, TCPConnector
+from aiohttp.client_exceptions import ServerDisconnectedError, ClientConnectionError
 from requests import get
-from asyncio import get_event_loop
+from asyncio import get_event_loop, gather, ensure_future, wait, sleep
 
 class AsyncRequestBase(object):
 
@@ -11,28 +12,54 @@ class AsyncRequestBase(object):
         self._url_dict = {url:None for url in url_list}
         self._url_list = url_list
         self.__loop = get_event_loop()
-        self._aiohttp_connector = TCPConnector(limit=100) # Limit for TCPConnector.
+        self._aiohttp_connector = TCPConnector(limit=1000) # Limit for TCPConnector.
         self._headers = headers
         self._proxy = proxy
 
     @property
     def result(self):
-        self.__loop.run_until_complete(
-            self.get_content()
-        )
+        try:
+            tasks = [ensure_future(self.get_content(url)) for url in self._url_list]
+            self.__loop.run_until_complete(
+                gather(*tasks)
+            )
+        except ServerDisconnectedError:
+            print("Request success")
+
         return self._url_dict
 
-    async def get_content(self):
-        async with ClientSession(connector=self._aiohttp_connector, headers=self._headers, requote_redirect_url=False) as session:
-            for url in self._url_list:
-                if self._proxy is None:
-                    async with session.get(url=url) as resp:
-                        data = await resp.text()
-                else:
-                    async with session.get(url=url) as resp:
-                        data = await self._proxy.async_proxy_request(url, "GET", session)
+    async def get_content(self, url):
+        async with ClientSession(connector=self._aiohttp_connector, headers=self._headers) as session:
+            if self._proxy is None:
+                async with session.get(url=url, timeout=10) as resp:
+                    data = await resp.text()
+            else:
+                async with session.get(url=url) as resp:
+                    data = await self._proxy.async_proxy_request(url, "GET", session)
+            self._url_dict[url] = data
+            return
 
-                self._url_dict[url] = data
+    @property
+    def postresult(self):
+        try:
+            tasks = [self.get_post_content(url[0], url[1]) for url in self._url_list]
+            self.__loop.run_until_complete(
+                gather(*tasks)
+            )
+        except (ClientConnectionError, ServerDisconnectedError) as e:
+            print("Request timeout =>", e)
+
+        return self._url_dict
+
+    async def get_post_content(self, url, payload):
+        async with ClientSession(connector=self._aiohttp_connector, headers=self._headers) as session:
+            await self.postfetch(session, url, payload)
+            await sleep(1)
+
+    async def postfetch(self, session, url, payload):
+        async with session.post(url=url, data=payload) as resp:
+            self._url_dict[(url, payload)] = await resp.text()
+
 
 class SyncRequestBase(object):
 
